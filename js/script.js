651ecbbf893fe93ffcc7c9228fd4450d1642a403
taxidrivermeow <fupsy1@gmail.com>
2023-02-05 14:34:49 +0200

    Request changed on fetch

--- js/script.js
+++ js/script.js
@@ -1,19 +1,12 @@
 (() => {
     const appKey = '0dab282f348f72b4a3ad5efd0e58ce12';
+    const showWeatherBtn = document.getElementById('show');
+    const weatherDiv = document.getElementById('weather');
 
-    $('#show').on('click', () => {
-        const city = $('#city').val().trim();
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${appKey}`;
-        const settings = {
-            success: (response)=>{
-                $('#weather').text(Math.floor(response.main.temp));
-            },
-            error: (err)=>{
-                console.log(err);
-            }
-        };
-
-        $.ajax(url, settings).done(()=>{});
-
+    showWeatherBtn.addEventListener('click', () => {
+        const city = document.getElementById('city').value;
+        const weatherProm = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${appKey}`);
+        weatherProm.then(res => res.json())
+                    .then(res => weatherDiv.innerText = Math.floor(res.main.temp));
     });
 })()
